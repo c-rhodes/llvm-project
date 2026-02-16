5ac09c2e258f1a24833b340f215b18b4f0a29753
Cullen Rhodes <cullen.rhodes@arm.com>
2026-02-16 11:52:18 +0000

    [AArch64] Use FastISel at O0

--- llvm/lib/Target/AArch64/AArch64TargetMachine.cpp
+++ llvm/lib/Target/AArch64/AArch64TargetMachine.cpp
@@ -159,7 +159,7 @@ static cl::opt<bool>
 static cl::opt<int> EnableGlobalISelAtO(
     "aarch64-enable-global-isel-at-O", cl::Hidden,
     cl::desc("Enable GlobalISel at or below an opt level (-1 to disable)"),
-    cl::init(0));
+    cl::init(-1));
 
 static cl::opt<bool>
     EnableSMEPeepholeOpt("enable-aarch64-sme-peephole-opt", cl::init(true),
